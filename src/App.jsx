b912b47907ac503372d13891539d662186ef8be8
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2023-06-21 14:16:11 +0530

    removed template code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
-import "./App.css";
 
 const App = () => {
   return <div>hi</div>;
