5e218554d38276bdae605a656f122b0db846ffa8
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2023-06-21 14:09:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import "./App.css";
+
+const App = () => {
+  return <div>hi</div>;
+};
+
+export default App;
