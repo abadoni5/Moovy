2b776a19c63ed754417e4b644e754c982d067a7b
Aayush Badoni <a.badoni@Aayushs-MacBook-Pro-2.local>
2023-06-25 16:13:49 +0530

    added movie view

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -3,17 +3,41 @@ import { useState, useEffect } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
 
-import { useGetMoviesQuery } from '../../services/TMDB';    
+import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
+
+
 
 const Movies = () => {
 
-    const { data } = useGetMoviesQuery();
+    const { data, error, isFetching } = useGetMoviesQuery();
 
-    console.log(data);
+    if (isFetching) { 
+        return (
+            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <CircularProgress size="4rem" />
+            </Box>
+        );
+    }
+
+    if (!data.results.length) { 
+        return (
+            <Box display="flex" alignItems="center" mt="20px">
+                <Typography variant='h4'>
+                    No movies that match that name.
+                    <br />
+                    Please search for something else.
+                </Typography>
+            </Box>
+
+        );
+    }
+
+    if(error) return 'An error has occurred: '
 
     return (
         <div>
-            hi
+            <MovieList movies={data} />
         </div>
     )
 }
